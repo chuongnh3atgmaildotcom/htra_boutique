16ca99bd03f85640acbfb69ebc29b446ef79181a
chuongnh <chuongnh3@gmail.com>
2022-10-06 13:51:13 +0700

    home: fix some type nagging

--- src/gatsby-theme-gallery/pages/index.tsx
+++ src/gatsby-theme-gallery/pages/index.tsx
@@ -4,14 +4,14 @@ import Header from "gatsby-theme-gallery/src/components/Header";
 import Footer from "gatsby-theme-gallery/src/components/Footer";
 import Gallery from "gatsby-theme-gallery/src/components/Gallery";
 import useSiteMetadata from "gatsby-theme-gallery/src/hooks/useSiteMetadata";
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import lodash from "lodash"
 import type { PageProps } from "gatsby"
-import type { ImageFormatType } from "gatsby-transformer-sharp"
+//import type { ImageFormatType } from "gatsby-transformer-sharp"
 import type CSS from 'csstype';
-import Img from "gatsby-image"
+import Img, { FluidObject } from "gatsby-image"
 
-const pcontainerstyle:CSS.Properties={display:'flex', flexDirection:'row', width:"100%"};
+const pcontainerstyle: CSS.Properties = { display: 'flex', flexDirection: 'row', width: "100%" };
 const HomePage = ({ data }: PageProps<ProductListType>) => {
   const siteMetadata = useSiteMetadata();
 
@@ -22,9 +22,11 @@ const HomePage = ({ data }: PageProps<ProductListType>) => {
       {console.log(data)}
       <div className="product" style={pcontainerstyle} >
         {data.allFireimage.edges.map(({ node: { id, name, desc, imgSrc, price, slug } }) => (
-          <div key={id} className="product-box" style={({ flexGrow: "1"})}>
-            {!!lodash.get(imgSrc, 'childImageSharp.fluid') && <Img fluid={imgSrc.childImageSharp.fluid} />}
-            <span>{name}</span>
+          <div key={id} className="product-box" style={({ flexGrow: "1" })}>
+            <Link to={"/sanpham/" + slug}>
+              {!!lodash.get(imgSrc, 'childImageSharp.fluid') && <Img fluid={imgSrc.childImageSharp.fluid} />}
+              <span>{name}</span>
+            </Link>
           </div>
         )
         )}
@@ -43,7 +45,11 @@ type ProductListType = {
           name: string,
           price: number,
           slug: string,
-          imgSrc: ImageFormatType
+          imgSrc: {
+            childImageSharp: {
+              fluid: FluidObject
+            }
+          }
         }
       }
     ]
